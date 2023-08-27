8e29a0833f80526f8cc6ea3ce2f8b3de5dc998c9
vega <devid367552@gmail.com>
2023-08-27 14:03:50 +0300

    Complete sensor logic

---
+++ src/main/java/edu/vega/rest/FirstRestApp/FirstRestAppApplication.java
@@ -0,0 +1,20 @@
+package edu.vega.rest.FirstRestApp;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FirstRestAppApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestAppApplication.class, args);
+	}
+
+}
